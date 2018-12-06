75bdca2c8d44e5599a4ad17d1a1ca547dd5466b8
Yusuke Iwaki <yusuke.iwaki@crowdworks.co.jp>
2018-12-07 02:03:24 +0900

    init

---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+      <router-link to="/">Home</router-link> |
+
+    これはAbout
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+      <router-link to="/about">About</router-link>
+
+    これはHome
+  </div>
+</template>
